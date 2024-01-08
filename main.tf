06774d41c54480447b7bad1d55c2c25692936317
luisfigler <luis-figler@outlook.com>
2024-01-08 12:33:11 -0700

    wek9

--- main.tf
+++ main.tf
@@ -3,29 +3,10 @@ provider "aws" {
 }
 
 
-resource "aws_iam_role" "example_role" {
-  name = "examplerole"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
+resource "aws_instance" "foo" {
+  ami           = "ami-00b8917ae86a424c9"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "TF-Intance"
+  }
 }
-EOF
-}
-
-# resource "aws_instance" "foo" {
-#   ami           = "ami-00b8917ae86a424c9"
-#   instance_type = "t2.micro"
-#   tags = {
-#     Name = "TF-Intance"
-#   }
-# }
